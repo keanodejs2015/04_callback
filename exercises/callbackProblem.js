062f54e66c69343fdae5a7aed50a48005e921831
Claus Bové <clbo@kea.dk>
2015-09-16 19:13:09 +0200

    .

--- exercises/callbackProblem.js
+++ exercises/callbackProblem.js
@@ -1,7 +1,8 @@
 // The following reads the text from a file and saves the content to a variable 'c'.
 // The problem is that when 'c' is printed in the console log statement
 // it is still undefined.
-// your job is to make this work.
+// Why is this not working as expected?
+// and you should make this work.
 
 var fs = require('fs');
 var c;
